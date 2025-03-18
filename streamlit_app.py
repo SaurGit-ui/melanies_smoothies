9783cf08125f6ba2f20b0488ffa40dd32a03a95d
SaurGit-ui <saurabh.barle@pwc.com>
2025-03-18 11:36:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,6 @@ if ingredients_list:
         st.subheader(fruits_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-        st.stop()
 
     #st.write(ingredients_string)
     
