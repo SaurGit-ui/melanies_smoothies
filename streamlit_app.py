1a0dccc5bae06e4dcaec08f2326c0c8e3c03e94e
SaurGit-ui <saurabh.barle@pwc.com>
2025-03-18 09:52:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,4 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiesfront_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiesfroot_response.json(), use_container_width=True)
